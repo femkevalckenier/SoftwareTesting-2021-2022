8202836ece29494f1fab648e2eb506e8ea33d2d1
dorisdeduitser <femke.valckenier2@student.odisee.be>
2021-09-27 15:11:51 +0200

    Update Course 1: Introduction to Java

--- JavaIntroduction/src/introduction/AddingMethods.java
+++ JavaIntroduction/src/introduction/AddingMethods.java
@@ -3,13 +3,26 @@ package introduction;
 public class AddingMethods {
 
 	public static void main(String[] args) {
-		String studentName = "John";
-		int studentScore = 90;
+		getGrades("John", 90);
+		getGrades("Tom", 50);
+	}
+
+	public static void getGrades(String studentName, int studentScore) {
 		String grade;
-		
-		if (studentScore >= 90) {
-			
+		if (studentScore >= 90 && studentScore < 100) {
+			grade = "A";
 		}
+		else if (studentScore >= 80 && studentScore < 90) {
+			grade = "B";
+		}
+		else if (studentScore >=70 && studentScore < 80) {
+			grade = "C";
+		}
+		else {
+			grade = "D";
+		}
+
+		System.out.println("Grade of " + studentName + " is: " + grade);
 	}
 
 }
